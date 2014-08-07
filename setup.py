06461f8d1184257a0472b002181e55a7f8930f95
MrLoom <MrLoom@soeinfachistdas.net>
2014-08-07 17:24:06 +0200

    updated setup.cfg and setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ setup(
     packages=['pyup'],
     entry_points={'console_scripts': ['pyup = pyup.pyup:pyup']},
 
+    zip_safe=True,
+    include_package_data=True,
+
     install_requires=['six', 'click', 'paramiko'],
 
     description='Simple file uploader',
@@ -22,15 +25,18 @@ setup(
 
     license='WTFPL',
     classifiers=[
+        'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
         'Natural Language :: English',
         'Operating System :: OS Independent',
+        'License :: Public Domain',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Utilities',
+        'Topic :: Communications :: File Sharing',
     ],
 )
